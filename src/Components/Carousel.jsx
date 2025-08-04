dd36dea461aa8a204347e0b951fe5bdfe2f0e95c
Sathar KP <satharkp292@google.com>
2025-08-04 16:59:54 +0530

    sds

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -64,12 +64,14 @@ const Carousel = () => {
               forceToAxis: true,
               sensitivity: 1,
             }}
-            className="max-w-[90%] lg:max-w-[80%] !pb-10 mt-16 lg:mt-0 swiper-center-pagination mx-auto flex flex-col items-center"
-          >
+            className="max-w-[90%] lg:max-w-[80%] !pb-10 mt-16 lg:mt-0 swiper-center-pagination mx-auto flex flex-col items-center">
+
               {ServiceData.map((item) => (
                 <SwiperSlide key={item.title} >
                   <div
                    className="relative mb-10 mx-auto group w-[68vw] rounded-sm max-w-[280px] lg:w-[300px] overflow-hidden shadow-lg hover:-translate-y-3 hover:shadow-2xl transition-all duration-300 ease-in-out min-h-[320px] lg:min-h-[400px]">
+
+
                     {/* Background Image */}
                     <img
                       src={item.backgroundImage || '/fallback.jpg'}
@@ -84,7 +86,7 @@ const Carousel = () => {
                     <div className="absolute inset-0 bg-black opacity-10 group-hover:opacity-0 transition-opacity duration-300 z-10 " />
 
                     {/* Foreground Content */}
-                    <div className="flex flex-col gap-6 relative z-20 text-white px-6 py-8 h-full w-full ">
+                    <div className="flex flex-col gap-6 relative z-20 text-white hover:font-bold px-6 py-8 h-full w-full ">
                       {item.icon && <item.icon className="text-[yellow] w-[30px] h-[23px] lg:h-[30px]" />}
                       <h3 className="text-lg font-bold mb-[-10px]">{item.title}</h3>
                       <p className="text-sm font-bold mb-[-20px]">{item.subHeading}</p>
