f763826945c37c8d090bb694b7b778cb21896725
Sathar KP <satharkp292@google.com>
2025-08-05 10:48:41 +0530

    map updates

--- src/Components/GoogleMapSection.jsx
+++ src/Components/GoogleMapSection.jsx
@@ -1,12 +1,17 @@
+import { useEffect } from "react";
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 const center = { lat: 10.804048122805051, lng: 76.18664964170154 };
 
 export default function GoogleMapSection() {
+  useEffect(() => {
+    window.dispatchEvent(new Event('resize'));
+  }, []);
+
   return (
     <LoadScript googleMapsApiKey="AIzaSyAQGIKzmVWKNZ7gdjOeJAmrPe2r7iBqSi0">
       <GoogleMap
-        mapContainerStyle={{ width: '100%', height: '100%' }}
+        mapContainerStyle={{ width: '100%', height: '300px' }}
         center={center}
         zoom={16}
       >
